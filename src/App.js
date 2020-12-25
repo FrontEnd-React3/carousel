e536d82010929f8d739635151738a73f4b446db4
intecfrontend <53944321+intecfrontend@users.noreply.github.com>
2020-12-25 08:48:03 +0100

    e

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Carousela from "./Carousel";
+import"./App.css"
+const App = () => {
+  return (
+    <div className="carou__container">
+    <div className="carou__cont">
+      <Carousela className="carou__wrap" />
+      </div>
+      </div>
+  );
+};
+
+export default App;
